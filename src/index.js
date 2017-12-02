9b41e44f26e33cd6e47cabc4e930855e07ad48a5
weathersboyle <rob@intrepid.io>
2017-12-02 13:56:44 -0500

    initial

---
+++ src/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class Form extends React.Component {
+  render() {
+    return (
+      <div className="game">
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(
+  <Form />,
+  document.getElementById('root')
+);
